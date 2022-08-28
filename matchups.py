906b73301213c413fe26160cd78041ba1117bca7
RyleeBuchert <ryleebuchert@gmail.com>
2022-08-28 14:47:45 -0500

    Adding player hands

--- matchups.py
+++ matchups.py
@@ -1,11 +1,48 @@
 import pandas as pd
 import string
 import os
+import json
 
 
 # Open player/team search files
 player_search = pd.read_csv("data\\player_search.csv", encoding = "ISO-8859-1")
 team_search = pd.read_csv("data\\team_search.csv")
+
+# Open player hand files
+with open('data\\batter_hands.json') as f:
+    batter_hands = json.load(f)
+with open('data\\pitcher_hands.json') as f:
+    pitcher_hands = json.load(f)
+
+
+# Switch team code to file version
+def fix_code(code):
+    if code == "ANA":
+        return("LAA")
+    elif code == "CHN":
+        return("CHC")
+    elif code == "CHA":
+        return("CHW")
+    elif code == "KCA":
+        return("KCR")
+    elif code == "LAN":
+        return("LAD")
+    elif code == "NYA":
+        return("NYY")
+    elif code == "NYN":
+        return("NYM")
+    elif code == "SDN":
+        return("SDP")
+    elif code == "SFN":
+        return("SFG")
+    elif code == "SLN":
+        return("STL")
+    elif code == "TBA":
+        return("TBR")
+    elif code == "WAS":
+        return("WSN")
+    else:
+        return(code)
 
 
 # Get all plate appearances for 2015-2021 seasons
@@ -20,6 +57,7 @@ for year in [2015,2016,2017,2018,2019,2020,2021]:
 
             # Get starting pitchers
             if parts[0] == "id":
+                game_code = "{}-{}-{}-{}".format(fix_code(parts[1][:3]), parts[1][3:7], parts[1][7:9], parts[1][9:11])
                 while parts[0] != "play":
                     line = f.readline().strip()
                     parts = line.split(",")
@@ -33,8 +71,10 @@ for year in [2015,2016,2017,2018,2019,2020,2021]:
             if parts[0] == "play":
                 # Get home/away and player ID's
                 batter = int(player_search.query(f"key_retro == '{parts[3]}'")['key_mlbam'])
+                batter_hand = batter_hands[f'{batter}']
                 batter_home = 0
                 pitcher = int(player_search.query(f"key_retro == '{home_pitcher}'")['key_mlbam'])
+                pitcher_hand = pitcher_hands[f'{pitcher}']
                 pitcher_home = 1
 
                 if parts[2] == "1":
@@ -83,10 +123,18 @@ for year in [2015,2016,2017,2018,2019,2020,2021]:
                                 play = play_parts[1][1]
                     
                     outcome = "h_" + play
+
+                    adv_ab = 0
+                    if batter_hand == 'S':
+                        adv_ab = 1
+                    elif batter_hand == 'R' and pitcher_hand == 'L':
+                        adv_ab = 1
+                    elif batter_hand == 'L' and pitcher_hand == 'R':
+                        adv_ab = 1
                     
                 # Ignore catcher interference and ambiguous singles.
                 if outcome not in {"h_C", "h_S"} and outcome != "":
-                    matchup_data.append([parts[1], batter, batter_home, pitcher, pitcher_home, outcome])
+                    matchup_data.append([game_code,parts[1],batter,batter_hand,batter_home,pitcher,pitcher_hand,pitcher_home,adv_ab,outcome])
 
             # Handle pitcher changes.
             if parts[0] == "sub":
@@ -100,10 +148,6 @@ for year in [2015,2016,2017,2018,2019,2020,2021]:
                         
         f.close()      
 
-    matchup_data = pd.DataFrame(matchup_data, columns=['inning','batter_id','batter_home','pitcher_id','pitcher_home','outcome'])
+    matchup_data = pd.DataFrame(matchup_data, columns=['game_code','inning','batter_id','batter_hand','batter_home',
+                                                       'pitcher_id','pitcher_hand','pitcher_home','adv_ab','outcome'])
     matchup_data.to_csv(f'data\\matchups\\{year}_matchups.csv')
-
-
-if __name__ == "__main__":
-
-    print()
